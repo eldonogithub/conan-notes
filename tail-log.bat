9732ab6e2d739d3dd6258e2920b074a85abb1dde
Eldon Olmstead <eldonogithub@blackperl.ca>
2022-11-12 21:51:08 -0400

    Init

---
+++ tail-log.bat
@@ -0,0 +1,3 @@
+cd "D:\Program Files (x86)\Steam\steamapps\common\Conan Exiles\ConanSandbox\Saved\Logs"
+Get-Content -Wait .\ConanSandbox.log
+Get-Content -Wait .\ConanSandbox.log | Select-String purge
